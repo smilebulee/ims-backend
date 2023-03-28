c9d4eeff757ad9a5d1f350cad5d3398c351064ec
smilebulee <bulee@infogen.co.kr>
2023-03-28 17:43:00 +0900

    jdk version

--- src/main/java/com/infogen/ims/configuration/WebConfig.java
+++ src/main/java/com/infogen/ims/configuration/WebConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://127.0.0.1:3000","http://218.151.225.142:3000");
     }
 }
